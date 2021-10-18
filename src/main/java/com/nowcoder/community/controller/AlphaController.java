ce1eb9aad4a989a23618732094a9b954913c2802
young <1522155475@qq.com>
2021-10-18 10:58:49 +0800

    community second commit

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -10,6 +10,6 @@ public class AlphaController {
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello(){
-        return "Hello Spring boot";
+        return "Hello Spring boot !!!";
     }
 }
